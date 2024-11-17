bd522976fff1191afd6382380532b22d35636f86
Matt Herold <mattherold@icloud.com>
2024-11-17 01:29:14 +0100

    .

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,10 +1,12 @@
+import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
     "stackbitVersion": "~0.6.0",
     "nodeVersion": "18",
     "ssgName": "custom",
     "contentSources": [],
-    "devCommand": "gem install jekyll bundler && bundle install && bundle exec jekyll serve --port {PORT}",
+    "devCommand": "bundle exec jekyll serve --port {PORT}",
+    "postInstallCommand": "npm i --no-save @stackbit/types && gem install jekyll bundler",
     experimental: {
     ssg: {
       name: "jekyll",
