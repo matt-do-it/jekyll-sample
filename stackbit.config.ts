2b0f1c9a921fc99210f6396b48eaea25e98f070b
Matt Herold <mattherold@icloud.com>
2024-11-17 01:43:56 +0100

    .

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,10 +1,41 @@
 import { defineStackbitConfig } from '@stackbit/types';
+import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
     "stackbitVersion": "~0.6.0",
     "nodeVersion": "18",
     "ssgName": "custom",
-    "contentSources": [],
+    contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["public/pages"],
+      models: [
+        {
+          name: "Page",
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "public/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true },
+                      { name: "content", type: "text", required: true }
+]
+        },
+                {
+          name: "People",
+          type: "data",
+          filePath: "public/data/{slug}.json",
+          fields: [
+            { name: "title", type: "string", required: true }
+          ]
+        }
+      ],
+      assetsConfig: {
+        referenceType: "static",
+        staticDir: "public",
+        uploadDir: "images",
+        publicPath: "/"
+      }
+    })
+  ],
     "devCommand": "bundle exec jekyll serve --port {PORT}",
     "postInstallCommand": "npm i --no-save @stackbit/types && gem install jekyll bundler",
     experimental: {
